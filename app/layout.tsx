37afe6c73f2f695a5a847e628cfe2c9458e5b812
AustinDevoreUWF <ard78@students.uwf.edu>
2025-10-31 06:41:03 -0500

    new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,46 @@
 import type { Metadata } from "next";
-import {Cormorant_Garamond,Poppins} from "next/font/google"
+import {
+  Cormorant_Garamond,
+  Poppins,
+  Manufacturing_Consent,
+} from "next/font/google";
 import "./globals.css";
 
-const cormorantGaramond =Cormorant_Garamond({
-    subsets:["latin"],
-    variable:"--font-cormorant-garamond",
-    weight:["400"],
-})
+const cormorantGaramond = Cormorant_Garamond({
+  subsets: ["latin"],
+  variable: "--font-cormorant-garamond",
+  weight: ["400"],
+});
+
 const poppins = Poppins({
-    subsets:["latin"],
-    variable:"--font-poppins",
-    weight:["400","600"],
-})
-export const metadata = {
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "600"],
+});
+
+const manufacturingConsent = Manufacturing_Consent({
+  subsets: ["latin"],
+  variable: "--font-manufacturing-consent",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
   title: "My Blog",
   description: "A blog to track what I'm learning.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${cormorantGaramond.variable} ${poppins.variable} bg-neutral-100`}>{children}</body>
+      <body
+        className={`${manufacturingConsent.variable} ${cormorantGaramond.variable} ${poppins.variable} bg-neutral-100`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function HomePage(){
   console.log(articles);
   return(
 <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
-  <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
-    <h1>austin's blog</h1>
+  <header className="font-manufacturing-consent font-light text-8xl text-neutral-900 text-center tracking-wider">
+    <h1>Austins Blog</h1>
   </header>
   <section className="md:grid md:grid-cols-2 flex flex-col gap-10">
     {articles !==null && Object.keys(articles).map((article)=>(
