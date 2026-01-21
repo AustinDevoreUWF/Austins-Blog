39556c2288f8eb1312d1f80cf0d1b79b21217977
AustinDevoreUWF <ard78@students.uwf.edu>
2026-01-21 15:52:24 -0600

    Added Analytics to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import {Cormorant_Garamond,Poppins, Cinzel} from "next/font/google"
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
@@ -37,6 +38,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProvider attribute="class">
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
